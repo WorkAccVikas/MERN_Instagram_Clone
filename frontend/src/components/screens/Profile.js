4264e4df3b8e2dd530794c949c90be4d8c42ceeb
WorkAccVikas <workaccvikas@gmail.com>
2023-08-01 14:34:54 +0530

    8. FD

--- frontend/src/components/screens/Profile.js
+++ frontend/src/components/screens/Profile.js
@@ -3,7 +3,93 @@ import React from "react";
 function Profile() {
   console.count("Profile");
 
-  return <div>Profile</div>;
+  return (
+    <div style={{ maxWidth: "550px", margin: "0 auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px 0",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+            alt="Random_Image"
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+          />
+        </div>
+        <div>
+          <h4>Vikas Chandlekar</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%",
+            }}
+          >
+            <h5>40 posts</h5>
+            <h5>40 followers</h5>
+            <h5>40 following</h5>
+          </div>
+        </div>
+      </div>
+
+      <div className="gallery">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=689&q=80"
+          alt="Gallery_Images"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
