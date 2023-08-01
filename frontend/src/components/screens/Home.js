6df4b01d422dee7d23a48cc5cff2025d76447036
WorkAccVikas <workaccvikas@gmail.com>
2023-08-01 15:00:22 +0530

    9. FD

--- frontend/src/components/screens/Home.js
+++ frontend/src/components/screens/Home.js
@@ -3,7 +3,63 @@ import React from "react";
 function Home() {
   console.count("Home");
 
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Vikas</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="card__image"
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>Vikas</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="card__image"
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>Vikas</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="card__image"
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title</h6>
+          <p>This is amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
