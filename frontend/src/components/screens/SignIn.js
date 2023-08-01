51873aaf13509ca940ffb0678d404cba90ed2135
WorkAccVikas <workaccvikas@gmail.com>
2023-08-01 13:28:02 +0530

    7. FD

--- frontend/src/components/screens/SignIn.js
+++ frontend/src/components/screens/SignIn.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SignIn() {
   console.count("SignIn");
 
   return (
     <div>
-      <h1>SignIn</h1>
+      <div className="myCard">
+        <div className="card auth-card input-field">
+          <h2>Instagram</h2>
+          <input type="text" placeholder="email" />
+          <input type="text" placeholder="password" />
+          <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+            Login
+          </button>
+          <h5>
+            <Link to="/signup">Don't have an account ?</Link>
+          </h5>
+        </div>
+      </div>
     </div>
   );
 }
