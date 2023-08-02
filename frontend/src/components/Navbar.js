7be3c0d29e2cd5fa3edf5ac5f0f417872832f7a7
WorkAccVikas <workaccvikas@gmail.com>
2023-08-02 21:17:31 +0530

    15. FD

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
-
+import { UserContext } from "../App";
 function Navbar() {
+  console.count("Navbar");
+
+  const { state, dispatch } = useContext(UserContext);
+
+  const renderList = () => {
+    if (state) {
+      return [
+        <li key="1">
+          <Link to="/profile">Profile</Link>
+        </li>,
+        <li key="2">
+          <Link to="/create">Create Post</Link>
+        </li>,
+      ];
+    } else {
+      return [
+        <li key="6">
+          <Link to="/signin">Signin</Link>
+        </li>,
+        <li key="7">
+          <Link to="/signup">Signup</Link>
+        </li>,
+      ];
+    }
+  };
+  
   return (
     <>
       <nav>
         <div className="nav-wrapper white">
-          <Link to="/" className="brand-logo left">
+          <Link to={state ? "/" : "/signin"} className="brand-logo left">
             Instagram
           </Link>
           <ul id="nav-mobile" className="right">
-            <li>
-              <Link to="/signin">Signin</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/create">Create Post</Link>
-            </li>
+            {renderList()}
           </ul>
         </div>
       </nav>
